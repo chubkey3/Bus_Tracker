c779c764f343e8a66b6f3f6c76b123d42bc39a6b
Chubkey <69558928+Ravager6969@users.noreply.github.com>
2022-03-08 14:37:49 -0800

    Update src.js

--- src.js
+++ src.js
@@ -78,21 +78,13 @@ function tConvert (time) {
 }
 
 async function call(){
-    //let response = await fetch('https://api.translink.ca/rttiapi/v1/stops/61112/estimates?apikey=8dBFRSNBr4HQDO6mi9Ee&count=1');
-    let response = await fetch('https://api.translink.ca/rttiapi/v1/stops/61112/estimates?apikey=8dBFRSNBr4HQDO6mi9Ee&count=1', {
-        headers: {
-            'content-type': "application/JSON"
-            }
-        }
-    );
-    let json = await response.json();
-    /*
+    let response = await fetch('https://api.translink.ca/rttiapi/v1/stops/61112/estimates?apikey=8dBFRSNBr4HQDO6mi9Ee&count=1');
     let data = await response.text();
 
-    //let test = parseXml(data);
+    let test = parseXml(data);
     let json_data =  parseXml(data);
     let json = JSON.parse(JSON.stringify(xmlToJson(json_data)));
-    */
+    
     const time = (json["NextBuses"]["NextBus"]["Schedules"]["Schedule"]["ExpectedLeaveTime"]["#text"]).split(" ")[0];
     const countdown = (json["NextBuses"]["NextBus"]["Schedules"]["Schedule"]["ExpectedCountdown"]["#text"]).split(" ")[0];
     const updated_time = new Date();
