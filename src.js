50b46d27f8b2787905e56f5e1eb2d5c2b8cc978b
Chubkey <69558928+Ravager6969@users.noreply.github.com>
2022-03-09 15:03:05 -0800

    Update src.js

--- src.js
+++ src.js
@@ -109,7 +109,7 @@ async function call(){
 function updatetime(){
     const updated_time = new Date();
     //var udisplay = document.getElementById('updated_display').textContent = "Last Updated: " + tConvert(addZero(updated_time.getHours()) + ":" + addZero(updated_time.getMinutes()) + ":" + addZero(updated_time.getSeconds()));
-    var udispalay = document.getElementById('updated_display').textContent = "Updated "+ Math.floor((updated_time-lasttime)/1000) + "Seconds Ago";
+    var udispalay = document.getElementById('updated_display').textContent = "Updated "+ Math.floor((updated_time-lasttime)/1000) + " Second(s) Ago";
 }
 
 function toggle(){
